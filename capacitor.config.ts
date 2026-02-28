02336701d3bb6f708a37ceb33fe14763d1a37643
majerom <nipunann0710@gmail.com>
2026-02-28 13:25:44 +0400

    Init ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ceyhallo.app',
+  appName: 'CeyHallo',
+  webDir: 'www'
+};
+
+export default config;
